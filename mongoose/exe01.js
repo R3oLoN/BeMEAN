1f2a2b2526fee7a6c1aca42a544de96960ff67e2
unknown <instrutor@CTB-WI7PCUA107.betha.com.br>
2014-07-22 20:24:37 -0300

    Aula Mongoose

---
+++ mongoose/exe01.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/workshop-criciuma');
+var db = mongoose.connection;
+db.on('error', function (err) {
+    console.log('Erro de conexao.', err)
+});
+db.once('open', function () {
+    console.log('Conexão aberta.')
+});
---
+++ read-file-sync.js
@@ -0,0 +1,8 @@
+"use strict"
+var fs = require("fs");
+console.log("Vou ler", Date.now());
+console.time("leitura");
+var file = fs.readFileSync("file.zip");
+console.log(file);
+console.timeEnd("leitura");
+console.log("Ja li", Date.now());
