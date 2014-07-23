ab9404ab16c4f000dbe9bb7c03b0b37b3ea319fd
unknown <instrutor@CTB-WI7PCUA107.betha.com.br>
2014-07-22 21:54:47 -0300

    CRUD completo

--- mongoose/app.js
+++ mongoose/app.js
@@ -1,50 +1,102 @@
-"use strict"
 var http = require("http");
+
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/workshop-criciuma');
+
 var db = mongoose.connection;
 db.on('error', function (err) {
-	console.log('Erro de conexao.', err);
+	console.log('Erro de conexao.', err)
 });
 db.once('open', function () {
-	console.log('Conexão aberta.');
+	console.log('Conexão aberta.')
 });
 
 var Schema = mongoose.Schema;
+
 var BeerSchema = new Schema({
 	name: {type: String, default: ''},
 	description: {type: String, default: ''},
 	alcohol: {type: Number, min: 0},
 	price: {type: Number, min: 0},
 	category: {type: String, default: ''},
-	created: {type: Date, default: Date.now}
+	created: {	type: Date, default: Date.now}
 });
+
 var Beer = mongoose.model('Beer', BeerSchema);
 
-http.createServer(function (request, response) {
-	response.writeHead(200, {"Content-Type": "text/html;charset=utf8"});
-	var url = request.url;
-	response.write("<h1>Hello World</h1>");
-	console.log('URL: ', url);
-	if (url === "/create") {
+var cb = function (err, data, res, msg) {
+	var _msg = msg;
+	if (err) {
+		_msg = 'Erro: ' + err;
+	}
+	console.log(_msg);
+	res.end(_msg);
+};
+
+var _beer = {
+	create: function(req, res){
 		var dados = {
-			name: 'Heineken',
-			description: 'Até q eh boazinha',
-			alcohol: 5.5,
+			name: 'Skol',
+			description: 'Mijo de Rato',
+			alcohol: 4.5,
 			price: 3,
-			category: 'lager'
+			category: 'pilsen'
 		}
 		var model = new Beer(dados);
 		model.save(function (err, data) {
-			if (err) {
-				console.log('Erro: ', err);
-			} else {
-				console.log('Cerveja Inserida', data);
-			}
-			response.end();
+			var msg = 'Cerveja ' + data.name + ' cadastrada com sucesso';
+			cb(err, data, res, msg);
 		});
-	}else{
-		response.end("Rota não encontrada!");
+	},
+	retrieve: function(req, res){
+		Beer.find({}, function (err, data) {
+			var msg = 'Cervejas listadas: ' + JSON.stringify(data);
+			cb(err, data, res, msg);
+		});
+	},
+	update: function(req, res){
+		var query = {name: 'Skol'};
+		var mod = {alcohol: 99};
+		var optional = {upsert: false, multi: true};
+		Beer.update(query, mod, function (err, data) {
+			var msg = 'Cerveja ' + query.name + ' alterada com sucesso';
+			cb(err, data, res, msg);
+		});
+	},
+	delete: function(req, res){
+		var query = {name: 'Skol'};
+		Beer.remove(query, function (err, data) {
+			var msg = 'Cervejas deletadas: ' + data;
+			cb(err, data, res, msg);
+		});
 	}
+};
+
+http.createServer(function (req, res) {
+	res.writeHead(200, {
+		"Content-Type": "text/plain;charset=utf8"
+	});
+
+	var url = req.url;
+	// res.write('Hello World');
+	console.log('URL: ', url);
+
+	switch (url) {
+	case '/create':
+		_beer.create(req,res);
+		break;
+	case '/retrieve':
+		_beer.retrieve(req,res);
+		break;
+	case '/update':
+		_beer.update(req,res);
+		break;
+	case '/delete':
+		_beer.delete(req,res);
+		break;
+	default:
+		res.end('Rota não encontrada!');
+	}
+
 }).listen(3000);
 console.log('Server running at http://localhost:3000/');
