6f6bcf068da6e9bc1168025bbfd8500407cb2723
unknown <instrutor@CTB-WI7PCUA107.betha.com.br>
2014-07-22 22:09:15 -0300

    Criação do módulo "Beer"

--- mongoose/app.js
+++ mongoose/app.js
@@ -1,28 +1,5 @@
 var http = require("http");
-
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/workshop-criciuma');
-
-var db = mongoose.connection;
-db.on('error', function (err) {
-	console.log('Erro de conexao.', err)
-});
-db.once('open', function () {
-	console.log('Conexão aberta.')
-});
-
-var Schema = mongoose.Schema;
-
-var BeerSchema = new Schema({
-	name: {type: String, default: ''},
-	description: {type: String, default: ''},
-	alcohol: {type: Number, min: 0},
-	price: {type: Number, min: 0},
-	category: {type: String, default: ''},
-	created: {	type: Date, default: Date.now}
-});
-
-var Beer = mongoose.model('Beer', BeerSchema);
+var Beer = require("./models/beers");
 
 var cb = function (err, data, res, msg) {
 	var _msg = msg;
@@ -41,7 +18,7 @@ var _beer = {
 			alcohol: 4.5,
 			price: 3,
 			category: 'pilsen'
-		}
+		};
 		var model = new Beer(dados);
 		model.save(function (err, data) {
 			var msg = 'Cerveja ' + data.name + ' cadastrada com sucesso';
